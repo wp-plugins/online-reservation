a71647f858f90eed64dc2f027f52ec95e47c2995
solweder <solweder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-04 11:38:41 +0000

    update files
    git-svn-id: https://plugins.svn.wordpress.org/online-reservation/trunk@1001692 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- online-reservation.php
+++ online-reservation.php
@@ -1,30 +1,30 @@
-<?php /*
-Plugin Name: Online Reservation
-Plugin URI: http://solweder.com/
-Description: allow you to manage and receive your business reservation online
-Author: Wahsidin Tjandra
-Author URI: http://solweder.com/about-me/
-Version: 1.0
-Donate link: 
-Tags: 
+<?php 
+/**
+ * Plugin Name: Online Reservation
+ * Plugin URI: http://solweder.com/
+ * Description: Allow you to manage and receive your business reservation online
+ * Version: 1.1
+ * Author: Wahsidin Tjandra
+ * Author URI: http://solweder.com/about-me/
+ * License:     GNU General Public License v2.0 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least: 3.6
+ * Tested up to: 4.0
+ *
+ * 
+ * 
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write
+ * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
- 
-Copyright 2010  solweder.com  (email : wahtjan@yahoo.com)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
-*/
   	
 	
 /*
